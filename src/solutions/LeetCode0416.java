bf18fe65c613eaa79199e3208019e3e14013c575
HDing-CN <dingh.loong@gmail.com>
2020-02-28 23:44:04 +0700

    leetcode 0416 dp

--- src/solutions/LeetCode0416.java
+++ src/solutions/LeetCode0416.java
@@ -43,16 +43,98 @@ public class LeetCode0416 {
         return dfs(cur - nums[numIndex], numIndex - 1, nums) || dfs(cur, numIndex - 1, nums);
     }
 
-    public boolean canPartition(int[] nums) {
+    public boolean canPartition_2DDP(int[] nums) {
+        Arrays.sort(nums);
 
+        int sum = 0, max = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            sum += nums[i];
+            max = Math.max(max, nums[i]);
+        }
 
-        return false;
+        if (sum % 2 != 0 || max > sum / 2) return false;
+
+        /**
+         * 这里的dp[i][j]表示的意思是，nums数组的前i个数字能否恰好填满大小为j的group
+         * 因此dp[i][0]一定为true
+         */
+        boolean[][] dp = new boolean[nums.length + 1][sum / 2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < nums.length + 1; i ++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i < nums.length + 1; i ++) {
+            for (int j = 1; j < sum / 2 + 1; j ++) {
+                /**
+                 * 第一种情况：如果前i-1个数字能填满容量为j的group，那么前i个也一定行
+                 * 第二种情况：如果前i - 1个数字能填满容量为j - nums[i]的group，那么
+                 * 前i个数字就能填满容量为j的group
+                 */
+                if (dp[i - 1][j]) {
+                    dp[i][j] = true;
+                } else if (j - nums[i - 1] >= 0 && dp[i - 1][j - nums[i - 1]]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < dp.length; i ++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        return dp[nums.length][sum / 2];
+    }
+
+    /**
+     * 因为二维DP中每一行只使用了上一行的信息，所以可以进行状态压缩，使用一维数组即可表示每一次循环用到的状态
+     * @param nums
+     * @return
+     */
+    public boolean canPartition_1DDP(int[] nums) {
+        Arrays.sort(nums);
+
+        int sum = 0, max = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            sum += nums[i];
+            max = Math.max(max, nums[i]);
+        }
+
+        if (sum % 2 != 0 || max > sum / 2) return false;
+
+        /**
+         * 这里的dp[i][j]表示的意思是，nums数组的前i个数字能否恰好填满大小为j的group
+         * 因此dp[i][0]一定为true
+         */
+        boolean[] dp = new boolean[sum / 2 + 1];
+        dp[0] = true;
+
+        for (int i = 1; i < nums.length + 1; i ++) {
+            /**
+             * 因为每一次循环用到的上一行数字一定在当前数字的左侧，
+             * 所以从后向前遍历就不会存在上一行元素被新值覆盖的问题
+             */
+            for (int j = sum / 2; j >= 0; j --) {
+                /**
+                 * 第一种情况：如果前i-1个数字能填满容量为j的group，那么前i个也一定行
+                 * 第二种情况：如果前i - 1个数字能填满容量为j - nums[i]的group，那么
+                 * 前i个数字就能填满容量为j的group
+                 */
+                if (dp[j]) {
+                    dp[j] = true;
+                } else if (j - nums[i - 1] >= 0 && dp[j - nums[i - 1]]) {
+                    dp[j] = true;
+                }
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+
+        return dp[sum / 2];
     }
 
     public static void main(String[] args) {
         LeetCode0416 leetCode0416 = LeetCode0416.getInstance();
-        int[] nums = {1,5,11,5};
-        System.out.println(leetCode0416.canPartition(nums));
+        int[] nums = {2,2,3,5};
+        System.out.println(leetCode0416.canPartition_1DDP(nums));
     }
-
 }
